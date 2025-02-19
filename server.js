b5b3fe9205935faea38bf179f3eca7f95f12aea2
Christian270304 <147045888+Christian270304@users.noreply.github.com>
2025-02-19 19:03:44 +0100

    remove unused variables for filename and directory in server.js

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ const app = express();
 const server = createServer(app);
 const PORT = process.env.PORT ||3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 console.log("CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
 
 // Configurar Passport
