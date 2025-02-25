bdb57d225dc1ed0b9c9265cfc4cc8041269703ef
Christian270304 <147045888+Christian270304@users.noreply.github.com>
2025-02-25 01:11:26 +0100

    remove unused assets: delete favicon, pyramid images, player/admin HTML files, and associated CSS

--- server.js
+++ server.js
@@ -1,31 +1,25 @@
 import express from 'express';
-import { fileURLToPath } from 'url';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import session from 'express-session';
-import http from 'http';
-import path from 'path';
+
 
 
 // Crear la aplicación de Express
 const app = express();
 const server = createServer(app);
-const PORT = 8080;
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+const PORT = process.env.PORT ||3000;
 
 
 // Configurar Passport
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8080/auth/google/callback'
+  callbackURL: 'http://pyramid-production-69a5.up.railway.app/auth/google/callback'
 },
-(accessToken, refreshToken, profile, done) => {
+(profile, done) => {
   if (profile._json.hd === 'sapalomera.cat') {
     return done(null, profile);
   } else {
@@ -35,11 +29,11 @@ passport.use(new GoogleStrategy({
 ));
 
 passport.serializeUser((user, done) => {
-done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser((obj, done) => {
-done(null, obj);
+  done(null, obj);
 });
 
 // Configurar Express
@@ -49,14 +43,15 @@ app.use(passport.session());
 
 // Rutas de autenticación
 app.get('/auth/google',
-passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/userinfo.email'] })
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/userinfo.email'] })
 );
 
 app.get('/auth/google/callback', 
-passport.authenticate('google', { failureRedirect: '/?error=No autorizado' }),
-(req, res) => {
-  res.redirect('/games');
-});
+  passport.authenticate('google', { failureRedirect: '/?error=No autorizado' }),
+  (req, res) => {
+    res.redirect('/games');
+  }
+);
 
 app.get('/logout', (req, res) => {
   req.logout();
@@ -71,26 +66,22 @@ function ensureAuthenticated(req, res, next) {
   res.redirect('/');
 }
 
-// Servir archivos estáticos
-app.use(express.static(path.join(__dirname, '../public')));
-
 // Rutas del servidor
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.redirect('http://pyramid.ctorres.cat/index.html');
 });
 
-app.get('/games', ensureAuthenticated , (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/games.html'));
+app.get('/games', ensureAuthenticated, (req, res) => {
+  res.redirect('http://pyramid.ctorres.cat/games.html');
 });
 
 app.get('/admin', ensureAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/admin.html'));
+  res.redirect('http://pyramid.ctorres.cat/admin.html');
 });
 
 app.get('/jugar', ensureAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/player.html'));
+  res.redirect('http://pyramid.ctorres.cat/player.html');
 });
-
 
 
 
@@ -102,8 +93,6 @@ app.get('/jugar', ensureAuthenticated, (req, res) => {
 //##############################################################################################//
 
 // Crear servidor HTTP para Socket.io
-
-
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -391,7 +380,7 @@ function addStoneToBase(team) {
   return stone;
 }
 
-// Iniciar el servidor http
+// Iniciar servidor de sockets
 server.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`⚡ Servidor corriendo en el puerto ${PORT}`);
 });
